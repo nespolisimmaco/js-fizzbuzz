d005b17820f7ac44e699de05308ec672137c49b1
nespolisimmaco <snespoli@yahoo.com>
2023-03-10 16:57:55 +0100

    Add: grid and boxes with elements inside created

--- js/script.js
+++ js/script.js
@@ -3,18 +3,24 @@
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 // Stampo in console i numeri da 1 a 100
+const gridElem = document.querySelector(".grid");
+
 for(let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         // Multipli di 3 e di 5
         console.log("FizzBuzz");
+        gridElem.innerHTML += `<div class="box">${"FizzBuzz"}</div>`
     } else if (i % 3 === 0) {
         // Multipli di 3
-        console.log("Fizz")
+        console.log("Fizz");
+        gridElem.innerHTML += `<div class="box">${"Fizz"}</div>`
     } else if (i % 5 === 0) {
         // Multipli di 5
         console.log("Buzz");
+        gridElem.innerHTML += `<div class="box">${"Buzz"}</div>`
     } else {
         // Tutti gli altri numeri
         console.log(i);
+        gridElem.innerHTML += `<div class="box">${i}</div>`
     }
 }
